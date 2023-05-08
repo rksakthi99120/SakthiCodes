69c99fbf7d19df68ee123fb14172e5f1b0878d6f
rksakthi123 <sakthivel.kaliyaperuml@xperi.com>
2023-05-08 15:07:03 +0530

    Added new cases

--- TestQA/src/main/java/com/test/api/TestApiModule.java
+++ TestQA/src/main/java/com/test/api/TestApiModule.java
@@ -13,15 +13,31 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class TestApiModule {
+	ApiResponse apiResponse=new ApiResponse();
 	
-	@Test(groups={"TestAPI","Module1"})
+	@Test(groups={"TestAPI"})
 	public void testGetAPI() {
 		ApiCall apiCall=new ApiCall();
 		ApiRequest apiRequest=new ApiRequest();
-		ApiResponse apiResponse=new ApiResponse();
-		apiResponse=apiCall.getApi("");
+		apiResponse=apiCall.getApi("http://universities.hipolabs.com/search?country=India");
 		
-		Assertion.assertEquals(200,apiResponse.getStatusCode());
+		Assertion.assertEquals(200,apiResponse.getStatusCode(),"Get University Api is not working",
+				"Test to verify the Get University API");
 	}
+	
+	@Test(groups={"TestAPI"},dependsOnMethods = {"testGetAPI"})
+	public void testGetAPIData() {
+		Assertion.assertEquals(true,apiResponse.getResponse().contains("Amrita Vishwa Vidyapeetham (Deemed University)")
+				,"Get University Api is not retrieving data properly",
+				"Test to verify the Get University API data validation");
+	}
+	
+	@Test(groups={"TestAPI"},dependsOnMethods = {"testGetAPI"})
+	public void testGetAPIDataFail() {
+		Assertion.assertEquals(true,apiResponse.getResponse().contains("Test University"),
+				"Get University Api is not retrieving data properly",
+				"Test to verify the Get University API data validation");
+	}
+
 
 }
